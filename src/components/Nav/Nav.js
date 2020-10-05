4a98a1bab33e786eb27f5cdd53055cb9d961612b
Ashvini Kumar <ashvini@esthenos.com>
2020-10-05 21:10:24 +0530

    basic setup and navigation bar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://static.tvmaze.com/images/tvm-header-logo.png"
+        alt="TVmaze Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="TVmaze Avatar"
+      />
+    </div>
+  );
+};
+
+export default Nav;
